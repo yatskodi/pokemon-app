df56675f064a586b0b5b00f84595001611d86af0
Yatsko Denys <denys_yatsko@Yatskos-Laptop.N4>
2024-07-03 13:24:11 +0300

    using env variables

--- src/client/api/index.ts
+++ src/client/api/index.ts
@@ -1,4 +1,4 @@
-export const API_HOST = 'http://localhost:3000';
+export const API_HOST = import.meta.env.VITE_API_HOST;
 
 export async function getPokemons() {
   const res = await fetch(`${API_HOST}/api`);
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { TanStackRouterVite } from "@tanstack/router-vite-plugin"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite({
-    routesDirectory: "src/client/routes/",
-  })],
+  plugins: [
+    react(),
+    TanStackRouterVite({
+      routesDirectory: 'src/client/routes/',
+    }),
+  ],
   server: {
     open: 'http://localhost:5173/',
   },
-})
+});
